4d5e8051a63dff6d1533a6dde8a056ad6d13b8f4
AlgorithmEncoder <algorithmencoder@gmail.com>
2023-08-25 13:07:06 +0200

    Sección eventos creada

--- JS/calendario.js
+++ JS/calendario.js
@@ -41,5 +41,57 @@ document.addEventListener("DOMContentLoaded", function () {
       calendarContainer.appendChild(weekRow);
     }
   }
-});
 
+
+  function showEventSection(date) {
+    const eventSection = document.getElementById("event-section");
+    const eventTitle = document.getElementById("event-section-title");
+    const eventList = document.getElementById("event-list");
+  
+    // Cambiar el título de la sección de eventos
+    const formattedDate = getFormattedDate(date);
+    eventTitle.textContent = "Eventos para el " + formattedDate;
+
+    // Obtener eventos para la fecha seleccionada
+    const events = getEvents(date);
+
+    // Limpiar la lista de eventos
+    eventList.innerHTML = "";
+
+    if (events.length > 0) {
+      events.forEach(function(event) {
+        const listItem = document.createElement("li");
+        listItem.className = "event-list-item";
+        listItem.textContent = event.time + " - " + event.event;
+        eventList.appendChild(listItem);
+      });
+    }
+    else {
+      const listItem = document.createElement("li");
+      listItem.textContent = "No hay eventos programados para este día.";
+      eventList.appendChild(listItem);
+    }
+
+    // Mostrar la sección de eventos
+    eventSection.style.display = "block";
+  }
+
+
+  function getFormattedDate(date) {
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return date.toLocaleDateString(undefined, options);
+  }
+
+  function getEvents(date) {
+    const selectedDate = date.toISOString().split("T")[0]; // Convertir a formato YYYY-MM-DD
+    const storedEventList = localStorage.getItem("eventList");
+    const eventList = storedEventList ? JSON.parse(storedEventList) : [];
+
+    const filteredEvents = eventList.filter(function(event) {
+      return event.date === selectedDate;
+    });
+
+    return filteredEvents;
+}
+showEventSection(new Date())
+});
